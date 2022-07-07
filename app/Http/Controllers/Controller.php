af0889071832845bedfc7b40d9019edc93241fdc
Vencelg <gazdavaclav@gmail.com>
2022-07-07 21:32:02 +0200

    [feat] added a response method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function response($data, int $status = 200, bool $errors = false): JsonResponse
+    {
+        return response()->json([
+            'version' => '1.0',
+            'errors' => $errors,
+            'data' => $data
+        ], $status);
+    }
 }

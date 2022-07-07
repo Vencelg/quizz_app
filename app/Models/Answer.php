ae8a017c4887e3cae319137faa5f4e6329059ff4
Vencelg <gazdavaclav@gmail.com>
2022-07-07 08:29:33 +0200

    [feat] add eloquent relations to existing models

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Answer extends Model
 {
@@ -18,4 +19,9 @@ class Answer extends Model
     protected $hidden = [
         'question_id',
     ];
+
+    public function question(): BelongsTo
+    {
+        return $this->belongsTo(Question::class);
+    }
 }

f808fcc0d06a2ecbb11e243e4aa3580ea1e4e8db
Vencelg <gazdavaclav@gmail.com>
2022-07-08 12:15:26 +0200

    [feat] add a functional quiz question CRUD

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreQuestionRequest;
+use App\Http\Requests\UpdateQuestionRequest;
+use App\Models\Question;
 use Illuminate\Http\Request;
 
 class QuestionController extends Controller
@@ -9,55 +12,84 @@ class QuestionController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function index()
+    public function index(): \Illuminate\Http\JsonResponse
     {
-        //
+        $questions = Question::all();
+
+        return $this->response($questions);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param StoreQuestionRequest $request
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function store(Request $request)
+    public function store(StoreQuestionRequest $request): \Illuminate\Http\JsonResponse
     {
-        //
+        $newQuestion = new Question([
+            'quiz_id' => $request->quiz_id,
+            'text' => $request->text,
+            'has_many_answers' => $request->has_many_answers,
+        ]);
+        $newQuestion->save();
+
+        return $this->response($newQuestion);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function show($id)
+    public function show(int $id): \Illuminate\Http\JsonResponse
     {
-        //
+        $question = Question::with(['answers', 'quiz'])->where('id', $id)->first();
+        if (!$question) {
+            return $this->response("Question with id: $id not found", 404, true);
+        }
+
+        return $this->response($question);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param UpdateQuestionRequest $request
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function update(Request $request, $id)
+    public function update(UpdateQuestionRequest $request, int $id): \Illuminate\Http\JsonResponse
     {
-        //
+        $question = Question::find($id);
+        if (!$question) {
+            return $this->response("Question with id: $id not found", 404, true);
+        }
+
+        $question->update($request->all());
+        $question->save();
+
+        return $this->response($question);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function destroy($id)
+    public function destroy(int $id): \Illuminate\Http\JsonResponse
     {
-        //
+        $question = Question::find($id);
+        if (!$question) {
+            return $this->response("Question with id: $id not found", 404, true);
+        }
+
+        $question->delete();
+
+        return $this->response([]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::apiResources([
-    'quizzes' => \App\Http\Controllers\QuizController::class
+    'quizzes' => \App\Http\Controllers\QuizController::class,
+    'questions' => \App\Http\Controllers\QuestionController::class,
 ]);
